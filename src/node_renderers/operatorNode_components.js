924753f5202803ae5882270bcd5f8f958a9c5361
Nic30 <nic30@seznam.cz>
2018-08-05 10:55:55 +0200

    additional operator nodes, style tweaks

--- src/node_renderers/operatorNode_components.js
+++ src/node_renderers/operatorNode_components.js
@@ -23,13 +23,38 @@ function negationCircle(root, x, y) {
     .attr("r" , "3");
 }
 
+function nodeCircleWithText(root, text) {
+  // width="25" height="25"
+  var tl = text.length;
+  if (tl > 2) {
+    throw new Error("Text too big for small node circle");
+  }  
+  var x = 8;
+  if (tl == 2)
+    x = 4;
+  
+  nodeCircle(root);
+  root.append("text")
+    .attr("x", x)
+    .attr("y", 16)
+    .text(text)
+}
+
 /**
  * Draw a AND gate symbol
  */
-function AND(root) {
+function AND(root, addName=true) {
 // width="30" height="25"
-  root.append("path")
+  var g = root.append("g")
+  g.append("path")
     .attr("d","M0,0 L0,25 L15,25 A15 12.5 0 0 0 15,0 Z");
+  g.attr("transform", "scale(0.8) translate(0, 3)")
+  if (addName)
+    root.append("text")
+      .attr("x", 8)
+      .attr("y", 16)
+      .text("&");
+  return g;
 }
 
 /**
@@ -37,8 +62,87 @@ function AND(root) {
  */
 function NAND(root) {
 // width="30" height="25"
-  AND(root);
+  AND(root, false);
   negationCircle(root, 34, 12.5);
+}
+
+
+var OR_SHAPE_PATH = "M3,0 A30 25 0 0 1 3,25 A30 25 0 0 0 33,12.5 A30 25 0 0 0 3,0";
+/**
+ * Draw a OR gate symbol
+ */
+function OR(root, addName=true) { 
+  // width="30" height="25"
+  var g = root.append("g")
+  g.append("path")
+    .attr("d", OR_SHAPE_PATH);
+  g.attr("transform", "scale(0.8) translate(0, 3)")
+  if (addName)
+    root.append("text")
+      .attr("x", 5)
+      .attr("y", 16)
+      .text("or")
+  return g;
+}
+
+/**
+ * Draw a NOR gate symbol
+ */
+function NOR(root) {
+  // width="33" height="25"
+  var g = OR(root, false);
+  g.append("circle")
+    .attr("cx", "34")
+    .attr("cy", "12.5")
+    .attr("r",   "3");
+  root.append("text")
+    .attr("x", 5)
+    .attr("y", 16)
+    .text("!|")
+}
+
+
+/**
+ * Draw a XOR gate symbol
+ */
+function XOR(root) {
+  var g = OR(root, false);
+  g.append("path")
+    .attr("d", "M0,0 A30 25 0 0 1 0,25")
+  root.append("text")
+    .attr("x", 8)
+    .attr("y", 16)
+    .text("^")
+
+  return g;
+}
+
+
+/**
+ * Draw a NXOR gate symbol
+ */
+function NXOR(root) {
+  // width="33" height="25"
+  var g = XOR(root);
+  negationCircle(g, 35, 12.5);
+  root.append("text")
+    .attr("x", 4)
+    .attr("y", 16)
+    .text("!^")
+}
+
+/**
+ * Draw a NOT gate symbol
+ */
+function NOT(root) {
+  // width="30" height="20"
+  root.append("path")
+    .attr("d", "M0,0 L0,20 L20,10 Z");
+  negationCircle(root, 23, 10);
+  root.append("text")
+    .attr("x", 8)
+    .attr("y", 16)
+    .text("!")
 }
 
 /**
@@ -54,121 +158,11 @@ function FF(root) {
    
   root.append("path")
     .attr("d","M0,2 L5,7 L0,12");
-}
-
-/**
- * Draw a equal operator symbol
- */
-function EQ(root) {
-  // width="25" height="25">
-  nodeCircle(root);
-  root.append("line")
-   .attr("x1", 7.5)
-   .attr("x2", 17.5)
-   .attr("y1", 10)
-   .attr("y2", 10);
-  root.append("line")
-   .attr("x1", 7.5)
-   .attr("x2", 17.5)
-   .attr("y1", 15)
-   .attr("y2", 15);
-}
-
-/**
- * Draw a not equal operator symbol
- */
-function NEQ(root) {
-  // width="25" height="25"
-  EQ(root);
-  root.append("line")
-    .attr("x1", 9)
-    .attr("x2", 16)
-    .attr("y1", 17)
-    .attr("y2", 8);
-}
-
-/**
- * Draw a OR gate symbol
- */
-function OR(root) { 
-  // width="30" height="25"
-  root.append("path")
-    .attr("d", "M0,25 L0,25 L15,25 A15 12.5 0 0 0 15,0 L0,0");
-  root.append("path")
-    .attr("d", "M0,0 A30 25 0 0 1 0,25");
-}
-
-/**
- * Draw a NOR gate symbol
- */
-function NOR(root) {
-  // width="33" height="25"
-  OR(root);
-  root.append("circle")
-    .attr("cx", "34")
-    .attr("cy", "12.5")
-    .attr("r",   "3");
-}
-
-
-/**
- * Draw a XOR gate symbol
- */
-function XOR(root) {
-  var g = root.append("g");
-  g.attr("transform", "scale(0.75) translate(0, 3.5)")
-  // width="33" height="25"
-  g.append("path")
-    .attr("d", "M3,0 A30 25 0 0 1 3,25 A30 25 0 0 0 33,12.5 A30 25 0 0 0 3,0");
-  g.append("path")
-    .attr("d", "M0,0 A30 25 0 0 1 0,25")
-  return g;
-}
-
-
-/**
- * Draw a NXOR gate symbol
- */
-function NXOR(root) {
-  // width="33" height="25"
-  XOR(root);
-  negationCircle(root, 35, 12.5);
-}
-
-/**
- * Draw a NOT gate symbol
- */
-function NOT(root) {
-  // width="30" height="20"
-  root.append("path")
-    .attr("d", "M0,0 L0,20 L20,10 Z");
-  negationCircle(root, 23, 10);
-}
-
-/**
- * Draw a substraction operator symbol
- */
-function SUB(root) {
-  // width="25" height="25"
-  nodeCircle(root)
-  root.append("line")
-    .attr("x1", 7.5)
-    .attr("x2", 17.5)
-    .attr("y1", 12.5)
-    .attr("y2", 12.5);
-}
-
-/**
- * Draw a addition operator symbol
- */
-function ADD(root) {
-  // width="25" height="25"
-  SUB(root);
-  root.append("line")
-    .attr("x1", 12.5)
-    .attr("x2", 12.5)
-    .attr("y1", 7.5)
-    .attr("y2", 17.5);
+ 
+  root.append("text")
+    .attr("x", 5)
+    .attr("y", 16)
+    .text("FF")
 }
 
 
@@ -182,15 +176,31 @@ export const SHAPES = {
   "XOR": XOR ,
   "NXOR":NXOR,
   
-  "ADD": ADD,
-  "SUB": SUB,
+  "ADD": function ADD(root) {
+	  nodeCircleWithText(root, ">=");
+  },
+  "SUB": function SUB(root) {
+	  nodeCircleWithText(root, "-");
+  },
   
-  "EQ":  EQ ,
-  // "LT":,
-  // "LE":,
-  // "GE":,
-  // "GT":,
-  "NEQ": NEQ,
+  "EQ": function EQ(root) {
+	  nodeCircleWithText(root, "=");
+  },
+  "NEQ": function NEQ(root) {
+	  nodeCircleWithText(root, "!=");
+  },
+  "LT": function LT(root) {
+	  nodeCircleWithText(root, "<");
+  },
+  "LE": function LE(root) {
+	  nodeCircleWithText(root, "<=");
+  },
+  "GE": function GE(root) {
+	  nodeCircleWithText(root, ">=");
+  },
+  "GT": function GT(root) {
+	  nodeCircleWithText(root, ">");
+  },
   
   "FF": FF,
 };
