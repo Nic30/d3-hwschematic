876a92c969e59bd989252098b8dce389dd73acb8
Nic30 <nic30@seznam.cz>
2018-08-08 14:53:41 +0200

    update tests for new jest

--- tests/d3-hwschematic.test.js
+++ tests/d3-hwschematic.test.js
@@ -1,8 +1,10 @@
 import * as d3 from "d3";
 import HwSchematic from '../src/d3-hwschematic';
-
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 describe('{unit}: Testing scheme rendering', () => {
+  global.document = new JSDOM().window.document;
   var svg = d3.select("body")
     .append("svg");
 
