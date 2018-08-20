654ab021e6f9b3bd1ffaffe98c0850103b185110
Nic30 <nic30@seznam.cz>
2018-08-20 21:18:04 +0200

    link wrap around links to simplify click

--- src/linkRenderer.js
+++ src/linkRenderer.js
@@ -1,17 +1,26 @@
 import {default as d3elk} from "./elk/elk-d3";
 
 export function renderLinks(root, edges) {
+    var linkWrap = root.selectAll(".link-wrap")
+      .data(edges)
+      .enter()
+      .append("path")
+      .attr("class", "link-wrap");
+
     var link = root.selectAll(".link")
-	  .data(edges)
-	  .enter()
-	  .append("path")
-	  .attr("class", "link")
+      .data(edges)
+      .enter()
+      .append("path")
+      .attr("class", "link");
+
     var junctionPoints = [];
     // apply edge routes
-    link.transition().attr("d", function(d) {
+    linkWrap.transition().attr("d", function(d) {
       var path = "";
-      if (!d.sections)
+      if (!d.sections) {
+    	  d._svgPath = "";
           return "";
+      }
       if (d.bendpoints || d.sections.length > 1) {
           throw new Error("NotImplemented");
       }
@@ -19,20 +28,25 @@ export function renderLinks(root, edges) {
           d.junctionPoints.forEach(function (jp) {
               junctionPoints.push(jp);
           });
-      return d3elk.section2svgPath(d.sections[0]);
+      d._svgPath = d3elk.section2svgPath(d.sections[0]);
+      return d._svgPath;
     });
-    var junctionPoints = root.selectAll(".junction-point")
-	  .data(junctionPoints)
-	  .enter()
-	  .append("circle")
-	  .attr("r", "3")
-	  .attr("cx", function(d) {
-	      return d.x;
-	  })
-	  .attr("cy", function(d) {
-	      return d.y;
-	  })
-	  .attr("class", "junction-point");
+    link.transition().attr("d", function(d) {
+    	return d._svgPath;
+    });
+    
+    var junctionPoint = root.selectAll(".junction-point")
+      .data(junctionPoints)
+      .enter()
+      .append("circle")
+      .attr("r", "3")
+      .attr("cx", function(d) {
+          return d.x;
+      })
+      .attr("cy", function(d) {
+          return d.y;
+      })
+      .attr("class", "junction-point");
 
-    return link;
+    return [link, linkWrap, junctionPoint];
 }
