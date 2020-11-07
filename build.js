e824c70b94b0107fadf35f3a9962b136ab0f7ae2
Nic30 <nic30@seznam.cz>
2020-11-07 10:48:49 +0100

    build.js: delegate stderr, stdout

--- build.js
+++ build.js
@@ -1,11 +1,16 @@
-//var util = require('util');
+var util = require('util');
 var exec = require('child_process').exec;
 var os = require('os');
 
-//function puts(error, stdout, stderr) { util.puts(stdout) }
+function puts(error, stdout, stderr) { 
+    util.puts(stdout);
+    if (error) {
+        util.puts(stderr);
+    } 
+ }
 
 // Run command depending on the OS
 if (os.type() === 'Windows_NT') 
-   exec("SET NODE_ENV=production && rollup -c");
+   exec("SET NODE_ENV=production && rollup -c", puts);
 else
-   exec("NODE_ENV=production rollup -c");
+   exec("NODE_ENV=production rollup -c", puts);
