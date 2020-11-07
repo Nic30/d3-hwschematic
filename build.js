a8de644bfb7047774d986ef807e325cdee369e0c
Nic30 <nic30@seznam.cz>
2020-11-07 10:36:43 +0100

    rm use of sys in build.js

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
-var sys = require('sys');
+//var util = require('util');
 var exec = require('child_process').exec;
 var os = require('os');
 
-function puts(error, stdout, stderr) { sys.puts(stdout) }
+//function puts(error, stdout, stderr) { util.puts(stdout) }
 
 // Run command depending on the OS
 if (os.type() === 'Windows_NT') 
-   exec("SET NODE_ENV=production && rollup -c", puts);
+   exec("SET NODE_ENV=production && rollup -c");
 else
-   exec("NODE_ENV=production rollup -c", puts);
+   exec("NODE_ENV=production rollup -c");
