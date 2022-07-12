3ffdb0d9224e48670b6b2bbe2777a9eb84802cae
sv12359 <s.vadovic9@gmail.com>
2022-07-12 23:49:37 +0200

    yosys convert top module children, nodes and ports

--- examples/yosysToD3Hwschematic.js
+++ examples/yosysToD3Hwschematic.js
@@ -37,27 +37,52 @@ ELK `LEdge` // only port - port connection
     "cssStyle": "stroke:red", // css style specification separated by ;
   }
 }
-{ // hyper LEdge
-  "id": "1119",
-  "sources": [
-    ["17", "343"]  // id of component, id of port
-  ],
-  "targets": [
-    [ "18", "346"],  // id of component, id of port
-    [ "21", "354"],
-  ],
-  "hwMeta": { // [d3-hwschematic specific]
-    "name": "wr_ptr",
-    "cssClass": "link-style0", // optional string, css classes separated by space
-    "cssStyle": "stroke:red", // css style specification separated by ;
-  }
-}
-
 */
-function makeLNode(name) {
 
-  return {
-    "id": "0", //generate, each component has unique id
+function makeLEdge(name, idCounter) {
+  return [{
+    "id": idCounter.toString(),
+    "sources": [
+
+    ],
+    "targets": [// id of LNode, id of LPort
+
+    ],
+    "hwMeta": { // [d3-hwschematic specific]
+      "name": name, // optional string, displayed on mouse over
+
+    }
+  }, idCounter + 1];
+}
+function makeLPort(name, direction, idCounter) {
+  if (direction === "output") {
+    var portSide = "EAST";
+  }
+  else if (direction === "input") {
+    var portSide = "WEST";
+  }
+  else {
+    throw new Error("Unknown direction " + direction);
+  }
+
+  return [{
+    "id": idCounter.toString(),
+    "hwMeta": { // [d3-hwschematic specific]
+      "name": name,
+    },
+    "direction": direction.toUpperCase(), // [d3-hwschematic specific] controlls direction marker
+    "properties": {
+      "side": portSide,
+      "index": 0 // The order is assumed as clockwise, starting with the leftmost port on the top side.
+      // Required only for components with "org.eclipse.elk.portConstraints": "FIXED_ORDER"
+    },
+    "children": [], // list of LPort, if the port should be collapsed rename this property to "_children"
+  }, idCounter + 1];
+}
+function makeLNode(name, idCounter) {
+
+  return [{
+    "id": idCounter.toString(), //generate, each component has unique id
     "hwMeta": { // [d3-hwschematic specific]
       "name": name, // optional str //mux
       "cls": "", // optional str //prazndy string
@@ -72,19 +97,96 @@ function makeLNode(name) {
     "edges": [],    // list of LEdge
     "children": [], // list of LNode, if the node should be collapsed rename this property
     // to "_children" and "edges" to "_edges"
-  };
+  }, idCounter + 1];
 }
 
 function yosysToD3Hwschematic(yosysJson) {
-  var output = makeLNode("top");
   var idCounter = 0;
+  var [output, idCounter] = makeLNode("root", idCounter);
   for (const [moduleName, moduleObj] of Object.entries(yosysJson.modules)) {
-    var n = makeLNode(moduleName);
+    var [n, idCounter] = makeLNode(moduleName, idCounter);
     //n.ports.push(...)
-    output.children.push(n)
+    output.children.push(n);
     console.log(moduleName);
+    for (const [portName, portObj] of Object.entries(moduleObj.ports)) {
+      var [port, idCounter] = makeLPort(portName, portObj.direction, idCounter);
+      n.ports.push(port);
+    }
+    for (const [cellName, cellObj] of Object.entries(moduleObj.cells)) {
+      var [subNode, idCounter] = makeLNode(cellName, idCounter);
+      n.children.push(subNode);
+      for (const [portName, direction] of Object.entries(cellObj.port_directions)) {
+        var [port, idCounter] = makeLPort(portName, direction, idCounter);
+        subNode.ports.push(port);
+      }
+    }
+
+    var edgeDict = {};
+
+    var nodeId = n.id;
+    var portI = 0;
+    for (const [portName, portObj] of Object.entries(moduleObj.ports)) {
+      var port = n.ports[portI++];
+      port.properties.index = portI;
+      var portId = port.id;
+      var index = portObj.bits[0]
+
+      var [edge, idCounter] = makeLEdge(portName, idCounter);
+
+      if (portObj.direction === "input") {
+        edge.sources.push([nodeId, portId]);
+
+      }
+      else if (portObj.direction === "output") {
+        edge.targets.push([nodeId, portId]);
+
+      }
+      else {
+        throw new Error("Unknown direction " + portObj.direction);
+      }
+      edgeDict[index] = edge;
+    }
+    for (var i = 0; i < n.children.length; ++i) {
+      const subNode = n.children[i];
+      var cell = moduleObj.cells[subNode.hwMeta.name];
+      var connections = cell.connections;
+      var portDirections = cell.port_directions;
+
+      if (connections === undefined) {
+        throw new Error("Cannot find cell for subnode" + subNode.hwMeta.name);
+      }
+      var portI = 0;
+      for (const [portName, netArray] of Object.entries(connections)) {
+        console.log(portI, subNode.ports);
+        var portObj = subNode.ports[portI++];
+        portObj.properties.index = portI;
+
+        var indexToSearch = netArray[0];
+        var edge = edgeDict[indexToSearch];
+
+        var direction = portDirections[portName];
+        if (direction === "output") {
+          edge.sources.push([subNode.id, portObj.id]);
+
+        }
+        else if (direction === "input") {
+          edge.targets.push([subNode.id, portObj.id]);
+
+        }
+        else {
+          throw new Error("Unknown direction " + direction);
+        }
+      }
+
+    }
+
+    for (const [edgeIndex, edge] of Object.entries(edgeDict)) {
+      n.edges.push(edge);
+    }
+
+
   }
-
-
+  n.hwMeta.maxId = idCounter - 1;
+  //console.log(JSON.stringify(output, null, 2));
   return output;
 }
